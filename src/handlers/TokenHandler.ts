bdff61399bf1457a36523fb3a62e2f58939314ba
unknown <ioko.peezaza@gmail.com>
2021-06-14 16:02:17 +0700

    1

--- src/handlers/TokenHandler.ts
+++ src/handlers/TokenHandler.ts
@@ -21,6 +21,7 @@ export const createToken = (UID: string, tokenVersion: number) => {
 export const sendToken = (res: Response, token: string) =>
     res.cookie(COOKIE_NAME!, token, {
         httpOnly: true,
+        sameSite: "lax",
     });
 
 export const verifyToken = (token: string) =>
